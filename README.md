32f0a4aadbb8ff03d9d718620e63f34407dc3e38
Eva <eva.bilski@hotmail.fr>
2020-08-31 16:47:45 +0200

    restructuration

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Ecomap
 
 ## Télécharger les données OSM
-1. Ouvrir le fichier overpass_query.txt
+1. Ouvrir le fichier osm/overpass_query.txt
 2. Choisir la zone à étudier et la remplacer dans le début de la requête 
 3. Copier la requête dans le formulaire de l'API Overpass : https://lz4.overpass-api.de/query_form.html
 
 ## Convertir les données OSM en fichiers CSV
-Executer le script python osm_convert/xml_to_csv_gouv.py, avec le fichier OSM en paramètre.
+Executer le script python osm/xml_to_csv_gouv.py, avec le fichier OSM en paramètre.
 Un nouveau dossier avec le même nom que le fichier passé en paramètre est créé. Ce dossier contient deux fichiers CSV : un fichier contenant les noeuds et un fichier contenant les relations.
 
 ## Importer les fichiers CSV dans Neo4j 
---
+++ osm/xml_to_csv_gouv.py
@@ -0,0 +1,113 @@
+import requests
+import csv, sys, os, warnings, io
+from xml.etree import ElementTree
+
+def clean(x):
+    #neo4j-import doesn't support: multiline (coming soon), quotes next to each other and escape quotes with '\""'
+    return x.replace('\n','').replace('\r','').replace('\\','').replace('"','')
+
+def clean_key(x):
+    return x.replace(':','-')
+
+# Creating tree
+print("Opening XML file")
+if len (sys.argv) < 2:
+    raise ValueError('No file given as argument')
+file_name = sys.argv[1]
+tree = ElementTree.parse(file_name)
+root = tree.getroot()
+
+# Output directory
+filename, file_extension = os.path.splitext(file_name)
+dir_name=filename + '_csv'
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+
+print("Retrieving all tags")
+node_tags= {':ID','osmId', 'lat', 'lon', 'street', 'loc-name', 'district', 'city', 'city-surface',
+            'city-centre', 'city-population', 'dpt', 'reg'}
+for node in root.findall('node'):
+    for tag in node.findall('tag'):
+        node_tags.add(clean_key(tag.get('k')))
+
+way_tags= {'osmId', ':TYPE', ':START_ID', ':END_ID'}
+for way in root.findall('way'):
+    for tag in way.findall('tag'):
+        way_tags.add(clean_key(tag.get('k')))
+
+# Nodes
+print("Reading & writing nodes in CSV file")
+all_nodes = set()
+with io.open(dir_name +'/nodes.csv', 'w', encoding="utf-8") as f:
+    w = csv.DictWriter(f, extrasaction='ignore', fieldnames=node_tags)
+    w.writeheader()
+    for node in root.findall('node'):
+        all_nodes.add(node.get('id'))
+        dct = dict.fromkeys(node_tags)
+        dct[':ID'] = node.get('id')
+        dct['osmId'] = node.get('id')
+        dct['lat'] = node.get('lat')
+        dct['lon'] = node.get('lon')
+
+        parameters = {"lat": node.get('lat'), "lon": node.get('lon')}
+        response = requests.get("https://api-adresse.data.gouv.fr/reverse/", params=parameters)
+        feat = response.json()['features']
+        if feat:
+            addr = feat[0]['properties']
+            dpt = addr['context'].split(',')        #num dpt + nom dpt + nom région
+            dct['dpt']=dpt[1]
+            dct['reg']=dpt[2]
+            dct['city']=addr['city']
+            print( addr['citycode'])
+            paramCity = {"code": addr['citycode'], 'fields':'surface,centre,population'}
+            responseCity = requests.get("https://geo.api.gouv.fr/communes/", params=paramCity)
+            print(responseCity.json())
+            if responseCity.json() :
+                responseCity = responseCity.json()[0]
+                dct['city-surface'] = responseCity['surface']
+                dct['city-centre'] = responseCity['centre']['coordinates']
+                dct['city-population'] = responseCity['population']
+            else:
+                print('xx')
+            dct['district']=addr.get('district')
+            dct['street']=addr.get('street')
+            if (dct['street'] == None):
+                dct['loc-name']=addr['name']
+
+        for tag in node.findall('tag'):
+            dct[clean_key(tag.get('k'))]=clean(tag.get('v'))
+        w.writerow(dct)
+
+# Ways
+complete_ways = True
+print("Reading & writing ways in CSV file")
+with io.open(dir_name +'/ways.csv', 'w', encoding="utf-8") as f:
+    w = csv.DictWriter(f, extrasaction='ignore', fieldnames=way_tags)
+    w.writeheader()
+    for way in root.findall('way'):
+        dct = dict.fromkeys(way_tags)
+        dct['osmId'] = way.get('id')
+        dct[':TYPE'] = 'NEXT_NODE'
+        nds = way.findall('nd')
+        for tag in way.findall('tag'):
+            dct[clean_key(tag.get('k'))]=clean(tag.get('v'))
+        for i in range(len(nds)-1) :
+            nd1 = nds[i].get('ref')
+            nd2 = nds[i+1].get('ref')
+            if (nd1 in all_nodes and nd2 in all_nodes) :     #check if node is in nodes
+                dct[':START_ID'] = nd1
+                dct[':END_ID'] = nd2
+                w.writerow(dct)
+            else :
+                complete_ways = False
+                print(nd1 + " " + nd2)
+                # reconstruire les chemins avec des noeuds hors zone ?
+
+if not complete_ways:
+    warnings.warn("Some nodes contained in the ways were not provided and were ignored")
+
+print()
+print("Move the generated files to the neo4j import directory and execute the following command in neo4j terminal :")
+print("bin/neo4j-admin import --nodes=import/nodes.csv --relationships=import/ways.csv") 
+print("-----------------------")
+print("bin\neo4j-admin.bat import --nodes=import\nodes.csv --relationships=import\ways.csv") 
